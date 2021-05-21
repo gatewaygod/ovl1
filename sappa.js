b22ebf919083ae61423b534fde320e89141e304b
gatewaygod <84291928+gatewaygod@users.noreply.github.com>
2021-05-21 14:34:14 -0400

    Create sappa.js

---
+++ sappa.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000 ;
+
+const server = http.createServer(function(req,res) {
+    
+res.end    
+})
+
+server.listen(port);
+console.log('Server is listening on port 3000');
